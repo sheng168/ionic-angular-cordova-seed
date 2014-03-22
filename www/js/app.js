03790c8678bdd282297db88467670387e8a5708e
Sheng Yu <shengyu@realnetworks.com>
2014-03-22 10:28:05 -0400

    add parse login/out

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,7 @@
 // Ionic Starter App
 
+Parse.initialize("CVKBJuIraBLT7fKeyFGWoPGWHeycTj6TSImraONF", "fxHHcIw6MiSfi9bjS26yeWasCNzs4GolrxIXMyLp");
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -7,7 +9,47 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'firebase'])
 
-.value('fbURL', 'https://find-cuh0fcgnzu.firebaseio-demo.com/User/cuH0FCgNzU/Install/')
+.value('fbURL', 'https://find-userId.firebaseio-demo.com/User/userId/Install/')
+
+.run(['$rootScope', function($scope) {
+    $scope.scenario = 'Sign up';
+    $scope.currentUser = Parse.User.current();
+
+    $scope.signUp = function(form) {
+        var user = new Parse.User();
+        user.set("email", form.email);
+        user.set("username", form.username);
+        user.set("password", form.password);
+
+        user.signUp(null, {
+            success: function(user) {
+                $scope.currentUser = user;
+                $scope.$apply();
+            },
+            error: function(user, error) {
+                alert("Unable to sign up:  " + error.code + " " + error.message);
+            }
+        });
+    };
+
+    $scope.logIn = function(form) {
+        Parse.User.logIn(form.username, form.password, {
+            success: function(user) {
+                $scope.currentUser = user;
+                $scope.$apply();
+                //alert("Success: " + user.id);
+            },
+            error: function(user, error) {
+                alert("Unable to log in: " + error.code + " " + error.message);
+            }
+        });
+    };
+
+    $scope.logOut = function(form) {
+        Parse.User.logOut();
+        $scope.currentUser = null;
+    };
+}])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -49,7 +91,8 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', '
       url: '/adopt',
       views: {
         'adopt-tab': {
-          templateUrl: 'templates/adopt.html'
+          templateUrl: 'templates/adopt.html',
+          controller: 'AuthCtrl'
         }
       }
     })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,50 @@
 angular.module('starter.controllers', [])
 
+// A simple controller that fetches a list of data from a service
+.controller('AuthCtrl', function($scope, $rootScope, DeviceService) {
+    // "Pets" is a service returning mock data (services.js)
+    //$scope.pets = PetService.all();
+
+    var user = $rootScope.currentUser;
+    if (user) {
+        $scope.user = {username: user.get('username')};
+    }
+})
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService, DeviceService) {
+.controller('PetIndexCtrl', function($scope, DeviceService, $firebase, fbURL, $rootScope) {
   // "Pets" is a service returning mock data (services.js)
   //$scope.pets = PetService.all();
+    var userId = 'userIdNone';
+        var user = $rootScope.currentUser;
 
-  $scope.pets = DeviceService;
+        if (user) {
+            userId = user.id;
+        }
+
+        var url = fbURL;
+        url = url.replace(/userId/g, userId);
+
+        var ref = new Firebase(url);
+        $scope.pets = $firebase(ref);
+
+//  $scope.pets = DeviceService;
 })
 
 
 // A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, $firebase, fbURL) {
-    var url = fbURL + $stateParams.petId;
+.controller('PetDetailCtrl', function($scope, $stateParams, $firebase, fbURL, $rootScope) {
+        var userId = 'userIdNone';
+        var user = $rootScope.currentUser;
+
+        if (user) {
+            userId = user.id;
+        }
+
+        var url = fbURL;
+        url = url.replace(/userId/g, userId);
+
+        url = url + $stateParams.petId;
         var ref = new Firebase(url);
         $scope.pet = $firebase(ref);
 
